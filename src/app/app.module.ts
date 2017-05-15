62140fcfa8222e02b202ef4b9889a89778842615
Jason Souryamath <jasonsouryamath@yahoo.com>
2017-05-15 16:22:32 -0700

    add all these files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AlertComponent } from './alert/alert.component';
+import { MenuComponent } from './menu.component';
+import { WeatherComponent } from './weather/weather.component';
+import { CONST_ROUTING } from './app.routing';
+import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap/dropdown/dropdown-config';
 
 
 @NgModule({
@@ -13,12 +19,23 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     NavComponent,
     FooterComponent,
+    AlertComponent,
+    MenuComponent,
+    WeatherComponent,
 
+
+
+ 
+    
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot(),
+    CONST_ROUTING,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/radio/radio.component.ts
@@ -0,0 +1,12 @@
+
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ngbd-buttons-radio',
+  templateUrl: './buttons-radio.html'
+})
+export class NgbdButtonsRadio {
+  model = 1;
+}
+
